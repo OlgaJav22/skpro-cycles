10326cea3fcf6d6ca69bdb3c4569d201613b886c
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-03 20:53:36 +0300

    Циклы интересные

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // задача 1
+        for (int i = 0; i<=10; i++){
+        System.out.println("Итерация цикла " + i);
+        }
+
+        //задача 2
+
+        for (int y =10; y>=1; y--) {
+            System.out.println("Итерация цикла " + y);
+        }
+
+        //задача 3
+        int u = 2;
+        for (int p = 0; p<17; p+=2) {
+            System.out.println("Итерация цикла " + p);
+        }
+    }
+}
