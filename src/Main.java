fea76f370d6005af5cbd303d7e035874548b8a67
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-05 20:41:36 +0300

    Все блоки готовы!

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,43 @@ public class Main {
         for (int x = 10; x>=-10; x--) {
             System.out.println("Интерация цикла " + x);
         }
+
+        // задача 1.2
+
+        for (int n = 1904; n<=2096; n+=4) {
+            System.out.println( n + " год является високосным");
+        }
+
+        // задача 2.2
+
+        for (int z = 7; z<=98; z+=7) {
+            System.out.println(z);
+        }
+
+        //задача 3.2
+
+        for (int s = 1; s<=512; s=s*2) {
+            System.out.println(s);
+        }
+
+        //задача 1.3
+
+        int deposit = 29000;
+        int total =0;
+        for (int j = 1; j<=12; j++){
+            total=total + deposit;
+            System.out.println("Месяц " + j + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        // задача 2.3
+
+        int bankDeposit = 29000;
+        int totalDeposit = 0;
+        for (int f = 1; f<=12; f++){
+            totalDeposit = totalDeposit + totalDeposit/100;
+            totalDeposit = totalDeposit + bankDeposit;
+            System.out.println("Месяц " + f + ", сумма накоплений равна " + totalDeposit + " рублей");
+        }
     }
+
 }
